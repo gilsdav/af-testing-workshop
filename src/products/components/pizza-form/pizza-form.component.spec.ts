fd46289d43716936a64a71f92adeaeadc37354bb
GILSON David <d.gilson@afelio.be>
2019-09-21 16:49:31 +0200

    comment unit tests

--- src/products/components/pizza-form/pizza-form.component.spec.ts
+++ src/products/components/pizza-form/pizza-form.component.spec.ts
@@ -10,42 +10,52 @@ describe('PizzaFormComponent', () => {
   let fixture: ComponentFixture<PizzaFormComponent>;
 
   beforeEach(async(() => {
-      TestBed.configureTestingModule({
-        declarations: [PizzaFormComponent, PizzaToppingsComponent],
-        imports: [ReactiveFormsModule],
-        // schemas: [NO_ERRORS_SCHEMA]
-      })/*.overrideComponent(PizzaFormComponent, {
+    // Init module
+    TestBed.configureTestingModule({
+      declarations: [PizzaFormComponent, PizzaToppingsComponent],
+      imports: [ReactiveFormsModule],
+      // schemas: [NO_ERRORS_SCHEMA]
+    })/*.overrideComponent(PizzaFormComponent, {
         set: { changeDetection: ChangeDetectionStrategy.Default }
       })*/
-        .compileComponents();
-      fixture = TestBed.createComponent(PizzaFormComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    })
+      .compileComponents();
+    // Get component
+    fixture = TestBed.createComponent(PizzaFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  })
   );
 
   it('should invalidate the form if no name', () => {
+    // Init form
     component.form.patchValue({});
+    // Expectations
     expect(component.form.valid).toBeFalsy();
   });
 
   it('should validate if name', () => {
+    // Init form
     component.form.patchValue({ name: 'Pizza' });
+    // Expectations
     expect(component.form.valid).toBeTruthy();
   });
 
   it('should display the create button if pizza does not exist', () => {
+    // Init form
     component.pizza = {} as Pizza;
-    component.ngOnChanges({pizza: component.pizza} as any);
+    component.ngOnChanges({ pizza: component.pizza } as any);
     fixture.detectChanges();
+    // Expectations
     const createButton = fixture.debugElement.query(By.css('#create_button'));
     expect(createButton).toBeTruthy();
   });
 
   it('should display the update button if pizza exists', () => {
+    // Init form
     component.pizza = { id: 1 } as Pizza;
-    component.ngOnChanges({pizza: component.pizza} as any);
+    component.ngOnChanges({ pizza: component.pizza } as any);
     fixture.detectChanges();
+    // Expectations
     const updateButton = fixture.debugElement.query(By.css('#update_button'));
     expect(updateButton).toBeTruthy();
   });
--- src/products/store/pizzas-state.spec.ts
+++ src/products/store/pizzas-state.spec.ts
@@ -24,41 +24,52 @@ describe('Pizza Store', () => {
   let backend: HttpTestingController;
 
   beforeEach(async(() => {
-      TestBed.configureTestingModule({
-        imports: [HttpClientTestingModule, NgxsModule.forRoot([PizzasState], { developmentMode: true })],
-        providers: [PizzasService, ToppingsService]
-      }).compileComponents();
-      store = TestBed.get(Store);
-      pizzasService = TestBed.get(PizzasService);
-      backend = TestBed.get(HttpTestingController);
-      store.reset(INIT_STATE);
-    })
+    // Init module
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, NgxsModule.forRoot([PizzasState], { developmentMode: true })],
+      providers: [PizzasService, ToppingsService]
+    }).compileComponents();
+    // Get services
+    store = TestBed.get(Store);
+    pizzasService = TestBed.get(PizzasService);
+    backend = TestBed.get(HttpTestingController);
+    // Init Store
+    store.reset(INIT_STATE);
+  })
   );
 
   it('should load toppings', () => {
+    // Mock backend
     backend.expectOne(`${environment.baseUrl}/toppings`).flush(['ham', 'cheese']);
     backend.verify();
+    // Expectations
     expect(store.selectSnapshot(PizzasState.toppings)).toEqual(['ham', 'cheese']);
   });
 
   it('should remove an existing pizza', () => {
+    // Init spyers
     spyOn(window, 'confirm').and.returnValue(true);
     spyOn(pizzasService, 'removePizza').and.returnValue(of());
+    // Expectations
     store.dispatch(new PizzasAction.Delete({ id: 1, name: 'Pizza1', toppings: [] })).subscribe(() => {
       expect(pizzasService.removePizza).toHaveBeenCalled();
     });
   });
 
   it('should not remove an existing pizza', () => {
+    // Init spyers
     spyOn(window, 'confirm').and.returnValue(false);
     spyOn(pizzasService, 'removePizza').and.returnValue(of());
+    // Expectations
     store.dispatch(new PizzasAction.Delete({ id: 1, name: 'Pizza1', toppings: [] })).subscribe(() => {
       expect(pizzasService.removePizza).not.toHaveBeenCalled();
     });
   });
 
   it('should update an existing pizza', () => {
+    // Init spyers
     spyOn(pizzasService, 'updatePizza').and.returnValue(of());
+    // Expectations
     store.dispatch(new PizzasAction.Update({ id: 1, toppings: ['Ham'] })).subscribe(() => {
       const pizzaState = store.selectSnapshot(state => state.pizzas);
       expect(pizzaState.pizzas[0].toppings.length).toBe(1);
@@ -67,7 +78,9 @@ describe('Pizza Store', () => {
   });
 
   it('should create pizza', () => {
+    // Init spyers
     spyOn(pizzasService, 'createPizza').and.returnValue(of());
+    // Expectations
     store.dispatch(new PizzasAction.Add({ id: 1, toppings: ['Ham'] })).subscribe(() => {
       expect(pizzasService.createPizza).toHaveBeenCalled();
     });
