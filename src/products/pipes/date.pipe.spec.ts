8af86349868cf7ca6b81d00b56485e1553e92c80
GILSON David <d.gilson@afelio.be>
2019-09-24 14:31:05 +0200

    update pipe test

--- src/products/pipes/date.pipe.spec.ts
+++ src/products/pipes/date.pipe.spec.ts
@@ -10,18 +10,19 @@ import { DatePipe } from '@angular/common';
 describe('Date Pipe', () => {
 
   let pipe: PipeTransform;
+  const dateToFormat = new Date('2019-09-21T19:38:04.395Z');
 
   beforeEach(() => {
     pipe = new DatePipe('en');
   });
 
   it('should return en date', () => {
-    const formatedDate = pipe.transform(new Date('2019-09-21T19:38:04.395Z'));
+    const formatedDate = pipe.transform(dateToFormat);
     expect(formatedDate).toBe('Sep 21, 2019');
   });
 
   it('should return formated date', () => {
-    const formatedDate = pipe.transform(new Date('2019-09-21T19:38:04.395Z'), 'dd/MM/yyyy');
+    const formatedDate = pipe.transform(dateToFormat, 'dd/MM/yyyy');
     expect(formatedDate).toBe('21/09/2019');
   });
 
